0be46adf83d454965121dc60e060caf6ff53d03b
Rene Treffer <treffer@measite.de>
2015-04-24 21:58:21 +0200

    [SPARK-6888] Make the jdbc driver handling user-definable
    Replace the DriverQuirks with JdbcDialect(s) (and MySQLDialect/PostgresDialect)
    and allow developers to change the dialects on the fly (for new JDBCRRDs only).
    
    Some types (like an unsigned 64bit number) can be trivially mapped to java.
    The status quo is that the RRD will fail to load.
    This patch makes it possible to overwrite the type mapping to read e.g.
    64Bit numbers as strings and handle them afterwards in software.
    
    JDBCSuite has an example that maps all types to String, which should always
    work (at the cost of extra code afterwards).

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/JDBCRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/JDBCRDD.scala
@@ -31,7 +31,7 @@ import org.apache.spark.util.Utils
 private[sql] object JDBCRDD extends Logging {
   /**
    * Maps a JDBC type to a Catalyst type.  This function is called only when
-   * the DriverQuirks class corresponding to your database driver returns null.
+   * the JdbcDialect class corresponding to your database driver returns null.
    *
    * @param sqlType - A field of java.sql.Types
    * @return The Catalyst type corresponding to sqlType.
@@ -41,7 +41,7 @@ private[sql] object JDBCRDD extends Logging {
       case java.sql.Types.ARRAY         => null
       case java.sql.Types.BIGINT        => LongType
       case java.sql.Types.BINARY        => BinaryType
-      case java.sql.Types.BIT           => BooleanType // Per JDBC; Quirks handles quirky drivers.
+      case java.sql.Types.BIT           => BooleanType // @see JdbcDialect for quirks
       case java.sql.Types.BLOB          => BinaryType
       case java.sql.Types.BOOLEAN       => BooleanType
       case java.sql.Types.CHAR          => StringType
@@ -94,7 +94,7 @@ private[sql] object JDBCRDD extends Logging {
    * @throws SQLException if the table contains an unsupported type.
    */
   def resolveTable(url: String, table: String, properties: Properties): StructType = {
-    val quirks = DriverQuirks.get(url)
+    val dialect = JdbcDialects.get(url)
     val conn: Connection = DriverManager.getConnection(url, properties)
     try {
       val rs = conn.prepareStatement(s"SELECT * FROM $table WHERE 1=0").executeQuery()
@@ -110,7 +110,7 @@ private[sql] object JDBCRDD extends Logging {
           val fieldSize = rsmd.getPrecision(i + 1)
           val nullable = rsmd.isNullable(i + 1) != ResultSetMetaData.columnNoNulls
           val metadata = new MetadataBuilder().putString("name", columnName)
-          var columnType = quirks.getCatalystType(dataType, typeName, fieldSize, metadata)
+          var columnType = dialect.getCatalystType(dataType, typeName, fieldSize, metadata)
           if (columnType == null) columnType = getCatalystType(dataType)
           fields(i) = StructField(columnName, columnType, nullable, metadata.build())
           i = i + 1
